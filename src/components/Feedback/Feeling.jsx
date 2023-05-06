ed69cac51cc0f162502da288ee346b4909a74277
Anders Boyum <Acboyum@gmail.com>
2023-05-06 10:35:36 -0500

    imported a few other packages, wrote up a quick TODO list

---
+++ src/components/Feedback/Feeling.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { Input, Button } from "@mui/base"
+
+function Feeling() {
+
+
+    return (
+        <>
+            <h3>How are you feeling today?</h3>
+        
+        </>
+    )
+}
